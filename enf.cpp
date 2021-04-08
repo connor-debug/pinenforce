a1ebdf6d5325e7cfa2e61012be927d31167c3ed6
connor-debug <70875324+connor-debug@users.noreply.github.com>
2021-04-08 13:57:13 -0400

    Add files via upload

--- enf.cpp
+++ enf.cpp
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "pin.H"
+#define MAXRET 60
+#define MAXNJMP 3
+#define MAXSJMP 3
 using std::ofstream;
 using std::string;
 using std::hex;
@@ -14,7 +17,10 @@ using std::endl;
 
 ofstream outFile;
 
-// A structure to represent a stack
+int rets = 0;
+int icount = 0;
+int bcount = 0;
+
 struct Stack {
     ADDRINT top;
     ADDRINT capacity;
@@ -40,14 +46,12 @@ ADDRINT isEmpty(struct Stack* stack)
     return stack->top == (ADDRINT)-1;
 }
  
-// Function to add an item to stack.  It increases top by 1
 void push(struct Stack* stack, ADDRINT item)
 {
     stack->array[++stack->top] = item;
     printf("%lu pushed to stack\n", item);
 }
  
-// Function to remove an item from stack.  It decreases top by 1
 ADDRINT pop(struct Stack* stack)
 {
     if (isEmpty(stack))
@@ -55,7 +59,6 @@ ADDRINT pop(struct Stack* stack)
     return stack->array[stack->top--];
 }
  
-// Function to return the top from stack without removing it
 ADDRINT peek(struct Stack* stack)
 {
     if (isEmpty(stack))
@@ -67,11 +70,17 @@ struct Stack* stack = createStack(20000);
 
 VOID checkStack(ADDRINT addr){
 	ADDRINT ch = pop(stack);
-	if(ch == addr) { printf("VALID RET!\n"); return; }
+	if(ch == addr) { printf("VALID RET!\n"); rets++;
+       		if(rets > MAXRET)
+			printf("4 returns in a row\n");
+		return;	}
 	while(!isEmpty(stack)){
 			ch = pop(stack);
 			if(ch == addr){
-				printf("VALID RET!");
+				printf("VALID RET!\n");
+				rets++;
+				if(rets > MAXRET)
+					printf("4 returns in a row");
 				return;
 			}
 		}
@@ -83,16 +92,40 @@ VOID checkStack(ADDRINT addr){
 VOID pushStack(ADDRINT addr)
 {
 	push(stack, addr);
+	rets = 0;
    // PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
    // outFile << "Address 0x" << hex << (unsigned long long)addr << dec << ":\t" << value << endl;
    // printf("found an indirect control flow transfer\n");
     return;
 }
 
+VOID checkBranch(){
+	if(icount > MAXNJMP){
+		icount = 0;
+		bcount++;
+	}
+	else {
+		bcount = 0;
+		icount = 0;
+	}
+	if(bcount > MAXSJMP){
+		printf("found sus jmp control flow transfer\n");
+		bcount = 0; //remove later	
+	}
+	return;
+}
+
+VOID countins(){icount++; printf("%d on the ins counter....\n", icount);}
+
 VOID Instruction(INS ins, VOID *v)
 {
 	ADDRINT nextAddress;
-	
+
+	INS_InsertCall(ins, IPOINT_BEFORE,
+			AFUNPTR(countins),
+			IARG_END);
+
+
     if (INS_IsCall(ins) && INS_IsControlFlow(ins))
     {
 	nextAddress = INS_NextAddress(ins);
@@ -113,12 +146,19 @@ VOID Instruction(INS ins, VOID *v)
 				IARG_BRANCH_TARGET_ADDR,
 				IARG_END);
 	}
+
+    if (INS_IsBranch(ins))
+	{
+		INS_InsertCall(ins,
+				IPOINT_BEFORE,
+				AFUNPTR(checkBranch),
+				IARG_END);
+	}
 }
 
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
                             "o", "enf.out", "specify output file name");
 
-// This function is called when the application exits
 VOID Fini(INT32 code, VOID *v)
 {
     outFile.close();
@@ -126,7 +166,7 @@ VOID Fini(INT32 code, VOID *v)
 
 INT32 Usage()
 {
-    cerr << "This tool dump heap memory information (global variable) ..." << endl;
+    cerr << "This tool is for enforcing control flow integrity." << endl;
     cerr << endl
          << KNOB_BASE::StringKnobSummary() << endl;
     return -1;
